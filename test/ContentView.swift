0a1f5223ef9700192f42b9853342eb6e4010019d
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-02-04 15:31:16 -0300

    init

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            
+            
+            
+        
     }
 }
 
