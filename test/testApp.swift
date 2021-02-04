1ebe292b23931a65a27e25ca2cbceb7fe9b0058a
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-02-04 14:36:06 -0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Lucas Melo on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
